b479e2b62e3fd70884e637fa402cd99a1f1dc586
Tiago Lino <taplino@gmail.com>
2020-10-31 16:44:26 -0100

    added multiple user

---
+++ app/src/main/java/com/medscript/asthmastair/Doctor.java
@@ -0,0 +1,15 @@
+package com.medscript.asthmastair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor);
+        setTitle("Doctor");
+    }
+}
---
+++ app/src/main/java/com/medscript/asthmastair/Patiente.java
@@ -0,0 +1,15 @@
+package com.medscript.asthmastair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Patiente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patiente);
+        setTitle("Patiente");
+    }
+}
