f8e6d9ff549363f8b7660e0dcb54690805810859
Tiago Lino <taplino@gmail.com>
2020-11-01 09:50:04 -0100

    changed ajuda ui

---
+++ app/src/main/java/com/medscript/asthmastair/MessageHelper.java
@@ -0,0 +1,21 @@
+package com.medscript.asthmastair;
+
+public class MessageHelper {
+    private String message;
+
+    public MessageHelper() {
+    }
+
+    public MessageHelper(String message) {
+
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- app/src/main/java/com/medscript/asthmastair/SintomasFragment.java
+++ app/src/main/java/com/medscript/asthmastair/SintomasFragment.java
@@ -7,15 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SintomasFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getActivity().setTitle("O meu diário de sintomas");
+
         return inflater.inflate(R.layout.fragment_sintomas, container, false);
     }
 }
