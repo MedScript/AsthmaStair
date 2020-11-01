7f9fdfd9cd7860f067a1a659494f5cfb563f432c
Tiago Lino <taplino@gmail.com>
2020-11-01 03:46:33 -0100

    added fragment ui

--- app/src/main/java/com/medscript/asthmastair/ConsultasFragment.java
+++ app/src/main/java/com/medscript/asthmastair/ConsultasFragment.java
@@ -1,64 +1,39 @@
 package com.medscript.asthmastair;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConsultasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ConsultasFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView marcarConsultas;
 
     public ConsultasFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConsultasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConsultasFragment newInstance(String param1, String param2) {
-        ConsultasFragment fragment = new ConsultasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consultas, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_consultas,
+                container, false);
+        marcarConsultas = rootView.findViewById(R.id.marcar_consulta);
+        marcarConsultas.setOnClickListener(v -> updateDetail());
+        return rootView;
     }
+
+    public void updateDetail() {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container,
+                new MarcarConsultasFragment()).commit();
+    }
+
 }
